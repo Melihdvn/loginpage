29c5852ed11db5bf5b184b47b83226ff6b2ec128
Melihdvn <melihdvn@gmail.com>
2024-07-10 18:03:31 +0300

    Add prettier settings

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    printWidth: 120,
+    arrowParens: 'avoid',
+    bracketSameLine: true,
+    bracketSpacing: false,
+    singleQuote: true,
+    trailingComma: 'all',
+    tabWidth: 4,
+    semi: true,
+};
